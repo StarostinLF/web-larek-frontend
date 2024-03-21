69b25078e21be0e9c7e1c5087c94ef836691e7a4
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2024-03-21 23:59:16 +0300

    Рефакторинг кода в utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -20,15 +20,12 @@ export function ensureAllElements<T extends HTMLElement>(
 	selectorElement: SelectorCollection<T>,
 	context: HTMLElement = document as unknown as HTMLElement
 ): T[] {
-	if (isSelector(selectorElement)) {
+	if (isSelector(selectorElement))
 		return Array.from(context.querySelectorAll(selectorElement)) as T[];
-	}
-	if (selectorElement instanceof NodeList) {
+	if (selectorElement instanceof NodeList)
 		return Array.from(selectorElement) as T[];
-	}
-	if (Array.isArray(selectorElement)) {
-		return selectorElement;
-	}
+	if (Array.isArray(selectorElement)) return selectorElement;
+
 	throw new Error(`Unknown selector element`);
 }
 
@@ -40,17 +37,15 @@ export function ensureElement<T extends HTMLElement>(
 ): T {
 	if (isSelector(selectorElement)) {
 		const elements = ensureAllElements<T>(selectorElement, context);
-		if (elements.length > 1) {
+		if (elements.length > 1)
 			console.warn(`selector ${selectorElement} return more then one element`);
-		}
-		if (elements.length === 0) {
+		if (elements.length === 0)
 			throw new Error(`selector ${selectorElement} return nothing`);
-		}
+
 		return elements.pop() as T;
 	}
-	if (selectorElement instanceof HTMLElement) {
-		return selectorElement as T;
-	}
+	if (selectorElement instanceof HTMLElement) return selectorElement as T;
+
 	throw new Error('Unknown selector element');
 }
 
@@ -58,6 +53,7 @@ export function cloneTemplate<T extends HTMLElement>(
 	query: string | HTMLTemplateElement
 ): T {
 	const template = ensureElement(query) as HTMLTemplateElement;
+
 	return template.content.firstElementChild.cloneNode(true) as T;
 }
 
@@ -67,8 +63,10 @@ export function bem(
 	modifier?: string
 ): { name: string; class: string } {
 	let name = block;
+
 	if (element) name += `__${element}`;
 	if (modifier) name += `_${modifier}`;
+
 	return {
 		name,
 		class: `.${name}`,
@@ -88,37 +86,31 @@ export function getObjectProperties(
 		.map(([name, prop]) => name);
 }
 
-/**
- * Устанавливает dataset атрибуты элемента
- */
+// Устанавливает dataset-атрибуты элемента
 export function setElementData<T extends Record<string, unknown> | object>(
 	el: HTMLElement,
 	data: T
 ) {
-	for (const key in data) {
-		el.dataset[key] = String(data[key]);
-	}
+	for (const key in data) el.dataset[key] = String(data[key]);
 }
 
-/**
- * Получает типизированные данные из dataset атрибутов элемента
- */
+// Получает типизированные данные из dataset-атрибутов элемента
 export function getElementData<T extends Record<string, unknown>>(
 	el: HTMLElement,
 	scheme: Record<string, Function>
 ): T {
 	const data: Partial<T> = {};
-	for (const key in el.dataset) {
+
+	for (const key in el.dataset)
 		data[key as keyof T] = scheme[key](el.dataset[key]);
-	}
+
 	return data as T;
 }
 
-/**
- * Проверка на простой объект
- */
+// Проверка на простой объект
 export function isPlainObject(obj: unknown): obj is object {
 	const prototype = Object.getPrototypeOf(obj);
+
 	return prototype === Object.getPrototypeOf({}) || prototype === null;
 }
 
@@ -126,32 +118,31 @@ export function isBoolean(v: unknown): v is boolean {
 	return typeof v === 'boolean';
 }
 
-/**
- * Фабрика DOM-элементов в простейшей реализации
- * здесь не учтено много факторов
- * в интернет можно найти более полные реализации
- */
+/* Фабрика DOM-элементов в простейшей реализации.
+Здесь не учтено много факторов в Интернете можно найти более полные реализации */
 export function createElement<T extends HTMLElement>(
 	tagName: keyof HTMLElementTagNameMap,
 	props?: Partial<Record<keyof T, string | boolean | object>>,
 	children?: HTMLElement | HTMLElement[]
 ): T {
 	const element = document.createElement(tagName) as T;
+
 	if (props) {
 		for (const key in props) {
 			const value = props[key];
-			if (isPlainObject(value) && key === 'dataset') {
+
+			if (isPlainObject(value) && key === 'dataset')
 				setElementData(element, value);
-			} else {
+			else {
 				// @ts-expect-error fix indexing later
 				element[key] = isBoolean(value) ? value : String(value);
 			}
 		}
 	}
 	if (children) {
-		for (const child of Array.isArray(children) ? children : [children]) {
+		for (const child of Array.isArray(children) ? children : [children])
 			element.append(child);
-		}
 	}
+
 	return element;
 }
