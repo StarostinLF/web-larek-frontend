cf55a6e179e8d703dd7cd4efec96f4d66cc6348d
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2024-03-22 01:45:34 +0300

    Написан и задокументирован Success.ts

---
+++ src/components/common/Success.ts
@@ -0,0 +1,28 @@
+import { Component } from '../base/Component';
+import { ensureElement } from '../../utils/utils';
+
+interface ISuccess {
+	total: number;
+}
+
+interface ISuccessActions {
+	onClick: () => void;
+}
+
+export class Success extends Component<ISuccess> {
+	protected _total: HTMLElement;
+	protected _close: HTMLElement;
+
+	constructor(container: HTMLElement, actions: ISuccessActions) {
+		super(container);
+
+		this._total = ensureElement<HTMLElement>(
+			'.order-success__description',
+			this.container
+		);
+		this._close = ensureElement<HTMLElement>('.state__action', this.container);
+
+		if (actions?.onClick)
+			this._close.addEventListener('click', actions.onClick);
+	}
+}
