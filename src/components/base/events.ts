3edc37c550bfe189e6e5f41bbf5a638b96778878
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2024-03-21 19:50:06 +0300

    Рефакторинг events.ts

--- src/components/base/events.ts
+++ src/components/base/events.ts
@@ -1,5 +1,3 @@
-/* Хорошая практика даже простые типы выносить в алиасы.
-Зато когда захотите поменять это достаточно сделать в одном месте */
 type EventName = string | RegExp;
 type Subscriber = Function;
 type EmitterEvent = {
@@ -9,16 +7,16 @@ type EmitterEvent = {
 
 export interface IEvents {
 	on<T extends object>(event: EventName, callback: (data: T) => void): void;
+
 	emit<T extends object>(event: string, data?: T): void;
+
 	trigger<T extends object>(
 		event: string,
 		context?: Partial<T>
 	): (data: T) => void;
 }
 
-/* Брокер событий, классическая реализация.
-В расширенных вариантах есть возможность подписаться на все события
-или слушать события по шаблону например */
+/* Брокер событий */
 export class EventEmitter implements IEvents {
 	_events: Map<EventName, Set<Subscriber>>;
 
@@ -28,9 +26,9 @@ export class EventEmitter implements IEvents {
 
 	// Установить обработчик на событие
 	on<T extends object>(eventName: EventName, callback: (event: T) => void) {
-		if (!this._events.has(eventName)) {
+		if (!this._events.has(eventName))
 			this._events.set(eventName, new Set<Subscriber>());
-		}
+
 		this._events.get(eventName)?.add(callback);
 	}
 
@@ -38,9 +36,9 @@ export class EventEmitter implements IEvents {
 	off(eventName: EventName, callback: Subscriber) {
 		if (this._events.has(eventName)) {
 			this._events.get(eventName)!.delete(callback);
-			if (this._events.get(eventName)?.size === 0) {
+
+			if (this._events.get(eventName)?.size === 0)
 				this._events.delete(eventName);
-			}
 		}
 	}
 
@@ -50,9 +48,8 @@ export class EventEmitter implements IEvents {
 			if (
 				(name instanceof RegExp && name.test(eventName)) ||
 				name === eventName
-			) {
+			)
 				subscribers.forEach((callback) => callback(data));
-			}
 		});
 	}
 
@@ -66,7 +63,7 @@ export class EventEmitter implements IEvents {
 		this._events = new Map<string, Set<Subscriber>>();
 	}
 
-	// Сделать коллбек триггер, генерирующий событие при вызове
+	// Сделать коллбэк триггер, генерирующий событие при вызове
 	trigger<T extends object>(eventName: string, context?: Partial<T>) {
 		return (event: object = {}) => {
 			this.emit(eventName, {
