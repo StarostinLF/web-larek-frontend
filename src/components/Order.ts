c4f622dbf1f6cc3a38e6b4b999ece56be179786d
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2024-03-23 16:05:57 +0300

    Рефакторинг Order.ts

--- src/components/Order.ts
+++ src/components/Order.ts
@@ -1,38 +1,21 @@
 import { Form } from './common/Form';
 import { IOrderForm } from '../types';
 import { IEvents } from './base/events';
-import { ensureElement } from '../utils/utils';
+import { ensureAllElements } from '../utils/utils';
 
 export class Order extends Form<IOrderForm> {
-	protected _card: HTMLButtonElement;
-	protected _cash: HTMLButtonElement;
+	protected _orderButton: HTMLButtonElement[];
 
 	constructor(container: HTMLFormElement, events: IEvents) {
 		super(container, events);
 
-		this._card = ensureElement<HTMLButtonElement>(
-			'button[name="card"]',
-			this.container
+		this._orderButton = ensureAllElements<HTMLButtonElement>(
+			'.button_alt',
+			container
 		);
-		this._cash = ensureElement<HTMLButtonElement>(
-			'button[name="cash"]',
-			this.container
+		this._orderButton.forEach((button) =>
+			button.addEventListener('click', () => (this.selected = button.name))
 		);
-		this._card.classList.add('button_alt-active');
-
-		this._card.addEventListener('click', () => {
-			this._card.classList.add('button_alt-active');
-			this._cash.classList.remove('button_alt-active');
-
-			events.emit('card:click');
-		});
-
-		this._cash.addEventListener('click', () => {
-			this._cash.classList.add('button_alt-active');
-			this._card.classList.remove('button_alt-active');
-
-			events.emit('cash:click');
-		});
 	}
 
 	set address(value: string) {
@@ -48,5 +31,12 @@ export class Order extends Form<IOrderForm> {
 	set phone(value: string) {
 		(this.container.elements.namedItem('phone') as HTMLInputElement).value =
 			value;
+	}
+
+	set selected(name: string) {
+		this._orderButton.forEach((button) =>
+			this.toggleClass(button, 'button_alt-active', button.name === name)
+		);
+		this.events.emit('payment:change', { name });
 	}
 }
